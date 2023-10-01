c145ba8f8f6b628d8778ab0d1ef4570db73a6df2
ᴀʙᴅᴜʟʟᴀʜ ᴀʟ ɴᴏᴍᴀɴ ᴘʀɪɴᴄᴇ <prince1000bd@gmail.com>
2023-10-02 02:13:48 +0600

    OOP - Conceptual Class 2 and Support Class(1 October) Contents are added

--- Module_4_Conceptual_Class_2_OOP_PART_2/Comparing_object/Comparing.php
+++ Module_4_Conceptual_Class_2_OOP_PART_2/Comparing_object/Comparing.php
@@ -31,11 +31,11 @@ $math1 = new Math( 1, 2 );
 // When two objects are created from the same class with same value, they are considered equal while checking with loose type check
 // var_dump( $math1 == $math2 );
 
-//When two objects are created from the same class with same value, they are considered different while checking with strict type check
+//When two objects are created from the same class with same value, they are considered different while checking with strict type check.
 // var_dump( $math1 === $math2 );
 
 // $math2 = new Math( 5, 9 );
-//When two objects are created from the same class with different value, they are considered different while checking with loose or strict type check
+//When two objects are created from the same class with different value, they are considered different while checking with loose or strict type check.
 // var_dump( $math1 == $math2 );
 // var_dump( $math1 === $math2 );
 
